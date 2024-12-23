8315265de75c5f90f7bcc519d816fc2bb071069d
牛洪法 <1242849166@qq.com>
2024-12-23 12:26:49 +0800

    fix

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -347,7 +347,7 @@ export const getWeek = (date: DateValue, lang?: LangValue): string => {
 		[6, "Sat"]
 	]);
 	var weeks = new Array(
-		"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"
+		"周日", "周一", "周二", "周三", "周四", "周五", "周六"
 	);
 	var week: string = lang.startsWith("zh-Han") ? weeks[day] : weekLang.get(day) as string;
 	return week;
